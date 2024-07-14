ebef0830fd72219f8f4e8d0997b75b1233900402
Muhammad Taufiq Bin Mohd Jaslan <105698083+taufiqjaslan@users.noreply.github.com>
2024-07-14 15:33:33 +0800

    fix navbar bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
-    const [scolled, setScolled] = useState('false');
+    const [scrolled, seScrolled] = useState('false');
 
     useEffect(() => {
         const onScroll = () => {
@@ -29,7 +34,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={''} alt="Logo" />
+                    <img src={logo} alt="Logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <span className="navbar-toggler-icon"></span>
@@ -41,9 +46,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className='social-icon'>
-                            <a href='#'><img src={ } alt=''></img></a>
-                            <a href='#'><img src={ } alt=''></img></a>
-                            <a href='#'><img src={ } alt=''></img></a>
+                            <a href="#"><img src={navIcon1} alt=''></img></a>
+                            <a href="#"><img src={navIcon2} alt=''></img></a>
+                            <a href="#"><img src={navIcon3} alt=''></img></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                     </span>
